fc86670bf8b91e4418b04f55058c9378cbfa82ae
Tuthuyvongan <Tuthuyvongan@gmail.com>
2024-01-19 10:56:25 +0700

    Update new API + code lấy api

---
+++ App.js
@@ -0,0 +1,32 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+async function saveData(data, filename) {
+  const filePath = path.join(
+    "C:/Users/S4T/Documents/GitHub/API/tramdomua",
+    `${filename}.json`
+  );
+  fs.writeFileSync(filePath, JSON.stringify(data));
+}
+
+async function main() {
+  const url = "https://csdlpcttapi.nuian.vn/api/tramdomua";
+  const initialData = await fetchData(url);
+
+  for (const item of initialData) {
+    const maTramAPI = item.MaTramAPI;
+    const data = await fetchData(
+      `https://csdlpcttapi.nuian.vn/api/tramdomua/${maTramAPI}`
+    );
+    saveData(data, maTramAPI);
+  }
+}
+
+main();
